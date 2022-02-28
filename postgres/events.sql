14b2c74b2bcff1a1e3717bb262760f5ea45199a5
Marco Lehmann <m99@posteo.de>
2022-02-28 12:42:01 +0100

    Move from ms to seconds and don’t export `updated_at` to not have high cardinality in the end

--- postgres/events.sql
+++ postgres/events.sql
@@ -10,9 +10,9 @@ INSERT INTO events (
   'source-1',
   'target-1',
   'payload-1',
-  now() - '0.012 seconds'::interval,
-  now() - '0.005 seconds'::interval,
-  now() - '0.005 seconds'::interval
+  now() - '0.12 hours'::interval,
+  now() - '0.05 hours'::interval,
+  now() - '0.05 hours'::interval
 );
 
 -- duration: 4 ms
@@ -27,9 +27,9 @@ INSERT INTO events (
   'source-2',
   'target-1',
   'payload-2',
-  now() - '0.015 seconds'::interval,
-  now() - '0.011 seconds'::interval,
-  now() - '0.011 seconds'::interval
+  now() - '0.15 hours'::interval,
+  now() - '0.11 hours'::interval,
+  now() - '0.11 hours'::interval
 );
 
 -- duration: 5 ms
@@ -44,7 +44,7 @@ INSERT INTO events (
   'source-3',
   'target-2',
   'payload-3',
-  now() - '0.006 seconds'::interval,
-  now() - '0.001 seconds'::interval,
-  now() - '0.001 seconds'::interval
+  now() - '0.06 hours'::interval,
+  now() - '0.01 hours'::interval,
+  now() - '0.01 hours'::interval
 );
